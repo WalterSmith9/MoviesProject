932b8066bd9599c7b0f105e9f0a75aa06064add2
DESKTOP-P390N85\Владимир <vova.kuznetsov4@mail.ru>
2022-08-01 18:25:28 +0300

    Sorting movies

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sort"
+	"strconv"
 	"time"
 )
 
@@ -13,6 +15,66 @@ var authentication = map[string]string{
 	"RandomGuy":"fgh",
 }
 
+var RBForm = []byte(`
+<form>
+  <p>Sort by:</p>
+  <div>
+    <input type="radio" id="sortChoice1"
+     name="sortMethod" value="name">
+    <label for="contactChoice1">Name</label>
+
+    <input type="radio" id="sortChoice2"
+     name="sortMethod" value="director">
+    <label for="contactChoice2">Director</label>
+
+    <input type="radio" id="sortChoice3"
+     name="sortMethod" value="year">
+    <label for="contactChoice3">Year</label>
+  </div>
+  <div>
+    <button type="submit">Submit</button>
+  </div>
+	</form>
+`)
+
+type film struct {
+	ID int
+	Name string
+	Year int
+	Director string
+}
+
+var films =[]film{
+	{1, "The Wolf Of Wall Street", 2013, "Martin Scorsese"},
+	{2, "The Hateful Eight", 2015, "Quentin Tarantino"},
+	{3, "Inception", 2010, "Christopher Nolan"},
+	{4, "The Departed", 2006, "Martin Scorsese"},
+	{5, "Enemy", 2013, "Denis Villeneuve"},
+	{6, "Nomadland", 2020, "Chloe Zhao"},
+}
+
+type movieList struct{
+	movies []film
+}
+
+func (mv movieList) sorted (metho string) []film{
+	if (metho == "name"){
+		sort.Slice(mv.movies, func(i, j int) bool {
+			return mv.movies[i].Name<mv.movies[j].Name
+		})
+	}
+	if (metho == "year"){
+		sort.Slice(mv.movies, func(i, j int) bool {
+			return mv.movies[i].Year<mv.movies[j].Year
+		})
+	}
+	if (metho == "director"){
+		sort.Slice(mv.movies, func(i, j int) bool {
+			return mv.movies[i].Director<mv.movies[j].Director
+		})
+	}
+	return mv.movies
+}
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
@@ -35,6 +97,7 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 	if loggedIn {
 		fmt.Fprintln(w, `<a href="/logout">logout</a>`)
 		fmt.Fprintln(w, "Welcome, "+session.Value)
+		fmt.Fprintln(w, `<br> <a href="/films">Фильмы</a>`)
 	} else {
 		//fmt.Fprintln(w, `<a href="/login">login</a>`)
 		http.Redirect(w, r, "/login", http.StatusFound)
@@ -60,11 +123,34 @@ func logoutPage(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
+func filmsPage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`
+	<html>	
+	<body>
+`))
+	movies := movieList{films}
+	method := r.FormValue("sortMethod")
+	if (r.FormValue("sortMethod") == ""){
+		method = "name"
+	}
+	sortedMv := movies.sorted(method)
+	for i := 0; i<len(films); i++{
+		fmt.Fprintln(w, sortedMv[i].Name + " " + sortedMv[i].Director + " " + strconv.Itoa(sortedMv[i].Year))
+		w.Write([]byte(`<br>`))
+	}
+	w.Write(RBForm)
+	w.Write([]byte(`
+	</body>
+	</html>
+`))
+}
+
 
 func main() {
 	http.HandleFunc("/", mainPage)
 	http.HandleFunc("/login", loginPage)
 	http.HandleFunc("/logout", logoutPage)
+	http.HandleFunc("/films", filmsPage)
 
 	staticHandler := http.StripPrefix(
 		"/static/",
