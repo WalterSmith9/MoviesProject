af3947710eadc81b6097aae159629ab6a65d6fad
DESKTOP-P390N85\Владимир <vova.kuznetsov4@mail.ru>
2022-08-09 17:43:12 +0300

    Registration&DB

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"sort"
@@ -9,10 +11,28 @@ import (
 	"time"
 )
 
-var authentication = map[string]string{
-	"Walter":"asd",
-	"Anthony":"zxc",
-	"RandomGuy":"fgh",
+type User struct{
+	id int
+	login string
+	password string
+}
+
+type film struct {
+	ID int
+	Name string
+	Year int
+	Director string
+}
+
+var database *sql.DB
+
+var films =[]film{
+	{1, "The Wolf Of Wall Street", 2013, "Martin Scorsese"},
+	{2, "The Hateful Eight", 2015, "Quentin Tarantino"},
+	{3, "Inception", 2010, "Christopher Nolan"},
+	{4, "The Departed", 2006, "Martin Scorsese"},
+	{5, "Enemy", 2013, "Denis Villeneuve"},
+	{6, "Nomadland", 2020, "Chloe Zhao"},
 }
 
 var RBForm = []byte(`
@@ -37,22 +57,6 @@ var RBForm = []byte(`
 	</form>
 `)
 
-type film struct {
-	ID int
-	Name string
-	Year int
-	Director string
-}
-
-var films =[]film{
-	{1, "The Wolf Of Wall Street", 2013, "Martin Scorsese"},
-	{2, "The Hateful Eight", 2015, "Quentin Tarantino"},
-	{3, "Inception", 2010, "Christopher Nolan"},
-	{4, "The Departed", 2006, "Martin Scorsese"},
-	{5, "Enemy", 2013, "Denis Villeneuve"},
-	{6, "Nomadland", 2020, "Chloe Zhao"},
-}
-
 type movieList struct{
 	movies []film
 }
@@ -76,31 +80,63 @@ func (mv movieList) sorted (metho string) []film{
 	return mv.movies
 }
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+
+	rows, err := database.Query("select * from usersdb.users")
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+	users := []User{}
+
+	for rows.Next(){
+		u := User{}
+		err := rows.Scan(&u.id, &u.login, &u.password)
+		if err != nil{
+			fmt.Println(err)
+			continue
+		}
+		users = append(users, u)
+	}
+	fmt.Fprintln(w, users)
+}
+
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		if (authentication[r.FormValue("login")] != r.FormValue("password")){
+		login := r.FormValue("login")
+		row:= database.QueryRow("select uPassword from usersdb.users where login = ?", login)
+		var password string
+		err := row.Scan(&password)
+		if err != nil{
+			fmt.Println(err)
 			http.Redirect(w, r, "/login", http.StatusFound)
+			return
 		}
-
+		if password != r.FormValue("password") {
+			fmt.Println("Wrong password")
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
 		expiration := time.Now().Add(10 * time.Hour)
 		cookie := http.Cookie{
 			Name: "session_id",
-			Value: r.FormValue("login"),
+			Value: login,
 			Expires: expiration,
 		}
 		http.SetCookie(w, &cookie)
+		fmt.Println("Right password")
 		http.Redirect(w, r, "/", http.StatusFound)
+		return
 	}
-
 	session, err := r.Cookie("session_id")
-	loggedIn := (err != http.ErrNoCookie)
+	loggedIn := err != http.ErrNoCookie
 	if loggedIn {
 		fmt.Fprintln(w, `<a href="/logout">logout</a>`)
-		fmt.Fprintln(w, "Welcome, "+session.Value)
-		fmt.Fprintln(w, `<br> <a href="/films">Фильмы</a>`)
+		fmt.Fprintln(w, "Welcome, " + session.Value)
+		fmt.Fprintln(w, `<br> <a href="/films">Фильмы</a><br>`)
 	} else {
-		//fmt.Fprintln(w, `<a href="/login">login</a>`)
 		http.Redirect(w, r, "/login", http.StatusFound)
+		return
 	}
 
 }
@@ -108,6 +144,55 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 func loginPage(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.ServeFile(w, r, "sources/loginPage.html")
+		return
+	}
+}
+
+func signupPage(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "sources/signupPage.html")
+	return
+}
+
+func registerPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		if r.FormValue("login") == "" || r.FormValue("password") == ""{
+			fmt.Println("Empty values")
+			http.Redirect(w, r, "/signup", http.StatusFound)
+			return
+		}
+
+		login := r.FormValue("login")
+		row := database.QueryRow("select login from usersdb.users where login = ?", login)
+		err := row.Scan()
+		if err != nil {
+			fmt.Println(err)
+		}
+		if err != sql.ErrNoRows {
+			fmt.Println("User already exists")
+			http.Redirect(w, r, "/signup", http.StatusFound)
+			return
+		}
+
+		var id int
+		row = database.QueryRow("SELECT MAX(id) FROM users")
+		err = row.Scan(&id)
+		if err != nil {
+			fmt.Println(err)
+		}
+		id++
+		_, err = database.Exec("INSERT INTO users (id, login, uPassword) VALUES (?, ?, ?)", id, login, r.FormValue("password"))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		expiration := time.Now().Add(10 * time.Hour)
+		cookie := http.Cookie{
+			Name: "session_id",
+			Value: login,
+			Expires: expiration,
+		}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w,r,"/", http.StatusFound)
 		return
 	}
 }
@@ -145,10 +230,18 @@ func filmsPage(w http.ResponseWriter, r *http.Request) {
 `))
 }
 
-
 func main() {
+	db, err := sql.Open("mysql", "root:Riptide_Embassy73@/usersdb")
+	if err != nil {
+		log.Println(err)
+	}
+	database = db
+
 	http.HandleFunc("/", mainPage)
+	http.HandleFunc("/index", IndexHandler)
 	http.HandleFunc("/login", loginPage)
+	http.HandleFunc("/signup", signupPage)
+	http.HandleFunc("/register", registerPage)
 	http.HandleFunc("/logout", logoutPage)
 	http.HandleFunc("/films", filmsPage)
 
