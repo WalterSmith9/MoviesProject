cccb6dd717a1b29716aec713051be93ac6364559
DESKTOP-P390N85\Владимир <vova.kuznetsov4@mail.ru>
2022-08-20 00:58:14 +0300

    Wishlist

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
@@ -9,19 +10,6 @@ import (
 	"sort"
 	"time"
 )
-
-type User struct{
-	id int
-	login string
-	password string
-}
-
-type film struct {
-	ID int
-	Name string
-	Year int
-	Director string
-}
 
 var database *sql.DB
 
@@ -35,27 +23,51 @@ var HTMLClose = []byte(`
 	</html>
 `)
 
-var RBForm = []byte(`
+var SortForm = []byte(`
 <form>
   <p>Sort by:</p>
   <div>
     <input type="radio" id="sortChoice1"
      name="sortMethod" value="name">
     <label for="contactChoice1">Name</label>
-
     <input type="radio" id="sortChoice2"
      name="sortMethod" value="director">
     <label for="contactChoice2">Director</label>
-
     <input type="radio" id="sortChoice3"
      name="sortMethod" value="year">
     <label for="contactChoice3">Year</label>
   </div>
   <div>
-    <button type="submit">Submit</button>
+    <button type="submit">Sort</button>
   </div>
 	</form>
 `)
+
+var FilterForm = []byte(`
+  <form>
+   <p>Choose between years:</p>
+   <div>
+	<input name="yearFrom" pattern="[0-9]{4}">
+	<b> - </b>
+	<input name="yearTo" pattern="[0-9]{4}">
+	<br><input type="submit" value="Fiter">
+   </div>
+  </form>
+`)
+
+type User struct{
+	id int
+	login string
+	password string
+	moviesID []int
+}
+
+type film struct {
+	ID int
+	Name string
+	Year int
+	Director string
+}
 
 type movieList struct{
 	movies []film
@@ -78,6 +90,15 @@ func (mv movieList) sorted (method string) []film{
 		})
 	}
 	return mv.movies
+}
+
+func isFound (slice []int, a int) bool {
+	for _, n := range slice {
+		if a == n {
+			return true
+		}
+	}
+	return false
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
@@ -113,6 +134,7 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, `<a href="/logout">logout</a>`)
 		fmt.Fprintln(w, "Welcome, " + session.Value)
 		fmt.Fprintln(w, `<br> <a href="/films">Фильмы</a><br>`)
+		fmt.Fprintln(w, `<br> <a href="/wishlist">Список желаемого</a><br>`)
 	} else {
 		http.Redirect(w, r, "/login", http.StatusFound)
 		return
@@ -190,15 +212,42 @@ func logoutPage(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
-func filmsPage(w http.ResponseWriter, r *http.Request) {
-	_, err := r.Cookie("session_id")
+func allFilmsPage(w http.ResponseWriter, r *http.Request) {
+	//checking if authorized
+	session, err := r.Cookie("session_id")
 	if err == http.ErrNoCookie {
 		http.Redirect(w, r, "/login", http.StatusFound)
 		return
 	}
-
-	w.Write(HTMLOpen)
-	rows, err := database.Query("Select * from usersdb.movies")
+	//collecting user's data
+	user :=User{}
+	user.login = session.Value
+	row := database.QueryRow("SELECT id FROM users WHERE login = ?", user.login)
+	row.Scan(&user.id)
+	//adding movie to wishlist
+	addFilm, err := strconv.Atoi(r.FormValue("addFilm"))
+	if err == nil {
+		_, err = database.Exec("INSERT INTO user_movie (userID, movieID) VALUES (?, ?);", user.id, addFilm)
+		if err != nil{
+			fmt.Println(err)
+		}
+	}
+	//getting movies from user's wishlist
+	rows, err := database.Query("Select movieID FROM user_movie WHERE userID=?", user.id)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for rows.Next(){
+		var mvID int
+		err := rows.Scan(&mvID)
+		if err != nil{
+			fmt.Println(err)
+			continue
+		}
+		user.moviesID = append(user.moviesID, mvID)
+	}
+	//getting movies from db and putting them into structure
+	rows, err = database.Query("Select * from usersdb.movies")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -213,17 +262,105 @@ func filmsPage(w http.ResponseWriter, r *http.Request) {
 		}
 		movies.movies = append(movies.movies, mv)
 	}
-
+	//setting filter parameters
+	yearFrom, yearTo := 0, 10000
+	if r.FormValue("yearFrom") != ""{
+		yearFrom,_ = strconv.Atoi(r.FormValue("yearFrom"))
+	}
+	if r.FormValue("yearTo") != ""{
+		yearTo,_ = strconv.Atoi(r.FormValue("yearTo"))
+	}
+	//setting sorting parameters and sorting right away
 	method := r.FormValue("sortMethod")
 	if r.FormValue("sortMethod") == "" {
 		method = "name"
 	}
 	sortedMv := movies.sorted(method)
+	//printing the list of movies according to filter parameters
+	w.Write(HTMLOpen)
+	w.Write([]byte(`<form id="addFilm"></form>`))
 	for i := 0; i<len(movies.movies); i++{
-		fmt.Fprintln(w, sortedMv[i].Name + " " + sortedMv[i].Director + " ", sortedMv[i].Year)
-		w.Write([]byte(`<br>`))
+		if sortedMv[i].Year >= yearFrom && sortedMv[i].Year <= yearTo {
+			if  isFound(user.moviesID, sortedMv[i].ID){
+				fmt.Fprintln(w,"+")
+			}else {
+				//pressing on that button will add movie to wishlist
+				w.Write([]byte(`<input type="submit" form="addFilm" name="addFilm" value=` + strconv.Itoa(sortedMv[i].ID) + `>`))
+			}
+			fmt.Fprintln(w, sortedMv[i].Name + " | " + sortedMv[i].Director + " ", sortedMv[i].Year)
+			w.Write([]byte(`<br>`))
+		}
 	}
-	w.Write(RBForm)
+	w.Write(SortForm)
+	w.Write(FilterForm)
+	w.Write(HTMLClose)
+}
+
+func wishlistPage(w http.ResponseWriter, r *http.Request) {
+	//checking if authorized
+	session, err := r.Cookie("session_id")
+	if err == http.ErrNoCookie {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+	//collecting user's data
+	user :=User{}
+	user.login = session.Value
+	row := database.QueryRow("SELECT id FROM usersdb.users WHERE login = ?", user.login)
+	row.Scan(&user.id)
+	//deleting movie from wishlist
+	deleteFilm, err := strconv.Atoi(r.FormValue("deleteFilm"))
+	if err == nil {
+		_, err = database.Exec("delete from usersdb.user_movie where userID = ? and movieID = ?;", user.id, deleteFilm)
+		if err != nil{
+			fmt.Println(err)
+		}
+	}
+	//getting movies from db and putting them into structure
+	rows, err := database.Query("SELECT movies.id, movies.name, movies.director, movies.year " +
+		"FROM usersdb.movies, usersdb.user_movie where user_movie.userID = ? and movies.id = user_movie.movieID",
+		user.id)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+	wishlist := movieList{}
+	for rows.Next(){
+		mv := film{}
+		err := rows.Scan(&mv.ID, &mv.Name, &mv.Director, &mv.Year)
+		if err != nil{
+			fmt.Println(err)
+			continue
+		}
+		wishlist.movies = append(wishlist.movies, mv)
+	}
+	//setting filter parameters
+	yearFrom, yearTo := 0, 10000
+	if r.FormValue("yearFrom") != ""{
+		yearFrom,_ = strconv.Atoi(r.FormValue("yearFrom"))
+	}
+	if r.FormValue("yearTo") != ""{
+		yearTo,_ = strconv.Atoi(r.FormValue("yearTo"))
+	}
+	//setting sorting parameters and sorting right away
+	method := r.FormValue("sortMethod")
+	if r.FormValue("sortMethod") == "" {
+		method = "name"
+	}
+	sortedMv := wishlist.sorted(method)
+	//printing the wishlist according to filter parameters
+	w.Write(HTMLOpen)
+	w.Write([]byte(`<form id="deleteFilm"></form>`))
+	for i := 0; i<len(wishlist.movies); i++{
+		if sortedMv[i].Year >= yearFrom && sortedMv[i].Year <= yearTo {
+			//pressing on that button will withdraw movie from wishlist
+			w.Write([]byte(`<input type="submit" form="deleteFilm" name="deleteFilm" value=` + strconv.Itoa(sortedMv[i].ID) + `>`))
+			fmt.Fprintln(w, sortedMv[i].Name + " | " + sortedMv[i].Director + " ", sortedMv[i].Year)
+			w.Write([]byte(`<br>`))
+		}
+	}
+	w.Write(SortForm)
+	w.Write(FilterForm)
 	w.Write(HTMLClose)
 }
 
@@ -239,8 +376,8 @@ func main() {
 	http.HandleFunc("/signup", signUpPage)
 	http.HandleFunc("/register", registerPage)
 	http.HandleFunc("/logout", logoutPage)
-	http.HandleFunc("/films", filmsPage)
-
+	http.HandleFunc("/films", allFilmsPage)
+	http.HandleFunc("/wishlist", wishlistPage)
 	staticHandler := http.StripPrefix(
 		"/static/",
 		http.FileServer(http.Dir("./sources")),
